b9f4dd4200a297c7d91dfe9966cb6f2fb6305f25
dimbinirina <aimerakotoarisoa53@gmail.com>
2026-04-24 14:08:51 +0300

    target link

--- js/custom.js
+++ js/custom.js
@@ -19,4 +19,51 @@ $(document).ready(function(){
     centerMode: true,
     centerPadding: '40px'
   });
+    const returnUp = document.querySelector(".ruturn-up");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) { // Afficher après 100px de scroll
+            returnUp.classList.add("show");
+        } else {
+            returnUp.classList.remove("show");
+        }
+    });
+    $("#to-header").on('click', function(event) {
+        if (event.isTrigger == undefined) {
+            $('html, body').animate({
+                scrollTop: $("#wrapper").offset().top - 1500
+            }, 200);
+        }
+    });
+    var nav_height = 20;
+    var sections = $('section');
+    var nav = $('.kl-navbar-menu');
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = $(this).attr('href');
+
+        if ($(target).length) {
+            $('html, body').animate({
+                scrollTop: $(target).offset().top - nav_height
+            }, 200);
+        }
+    });
+
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+
+        sections.each(function() {
+            var top = $(this).offset().top - nav_height;
+            var bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
 });
